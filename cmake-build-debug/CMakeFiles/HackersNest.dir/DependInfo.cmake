bd3b34d940ad5d21080b9c9b258b6a5c5b3c6d94
toastedwaffle <kabir.verma.8163>
2021-09-18 14:27:33 -0400

    Making the shit work on the m1

---
+++ cmake-build-debug/CMakeFiles/HackersNest.dir/DependInfo.cmake
@@ -0,0 +1,120 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/ftp/Ftp.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/ftp/Ftp.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/island/Island.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/island/Island.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/joystick/Joystick.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/joystick/Joystick.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/opengl/OpenGL.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/opengl/OpenGL.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/pong/Pong.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/pong/Pong.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/shader/Shader.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/shader/Shader.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/sockets/Sockets.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/sockets/Sockets.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/sockets/TCP.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/sockets/TCP.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/sockets/UDP.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/sockets/UDP.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/sound-capture/SoundCapture.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/sound-capture/SoundCapture.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/sound/Sound.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/sound/Sound.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/voip/Client.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/voip/Client.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/voip/Server.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/voip/Server.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/voip/VoIP.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/voip/VoIP.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/win32/Win32.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/win32/Win32.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/SFML-2.5.0/examples/window/Window.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/SFML-2.5.0/examples/window/Window.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/Game/GameBoard.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/Game/GameBoard.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/EntitySystem/Component.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/EntitySystem/Component.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/AnimationComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/AnimationComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/ButtonComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/ButtonComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/CollidableComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/CollidableComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/CollidablePhysicsComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/CollidablePhysicsComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/ParticleComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/ParticleComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/ParticleEmitterComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/ParticleEmitterComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/RenderComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/RenderComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/SoundComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/SoundComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/SpriteRenderComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/SpriteRenderComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/TextRenderComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/EntitySystem/Components/TextRenderComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/EntitySystem/Entity.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/EntitySystem/Entity.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/GameEngineMain.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/GameEngineMain.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/Util/AnimationManager.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/Util/AnimationManager.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/Util/ButtonManager.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/Util/ButtonManager.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/Util/CameraManager.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/Util/CameraManager.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/Util/CollisionManager.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/Util/CollisionManager.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/Util/SoundManager.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/Util/SoundManager.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/GameEngine/Util/TextureManager.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/GameEngine/Util/TextureManager.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_Blank/Source/main.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_Blank/Source/main.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/Game/GameBoard.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/Game/GameBoard.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/Game/GameComponents/PlayerCameraComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/Game/GameComponents/PlayerCameraComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/Game/GameComponents/PlayerMovementComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/Game/GameComponents/PlayerMovementComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/Game/GameComponents/PlayerSoundComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/Game/GameComponents/PlayerSoundComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/Game/GameEntities/ObstacleEntity.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/Game/GameEntities/ObstacleEntity.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/Game/GameEntities/PlayerEntity.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/Game/GameEntities/PlayerEntity.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Component.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Component.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/AnimationComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/AnimationComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/ButtonComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/ButtonComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/CollidableComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/CollidableComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/CollidablePhysicsComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/CollidablePhysicsComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/ParticleComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/ParticleComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/ParticleEmitterComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/ParticleEmitterComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/RenderComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/RenderComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/SoundComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/SoundComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/SpriteRenderComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/SpriteRenderComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/TextRenderComponent.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components/TextRenderComponent.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Entity.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/EntitySystem/Entity.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/GameEngineMain.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/GameEngineMain.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/Util/AnimationManager.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/Util/AnimationManager.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/Util/ButtonManager.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/Util/ButtonManager.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/Util/CameraManager.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/Util/CameraManager.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/Util/CollisionManager.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/Util/CollisionManager.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/Util/SoundManager.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/Util/SoundManager.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/GameEngine/Util/TextureManager.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/GameEngine/Util/TextureManager.cpp.o"
+  "/Users/toastedwaffle/CLionProjects/HackersNest/UbiGame_FlappBird/Source/main.cpp" "/Users/toastedwaffle/CLionProjects/HackersNest/cmake-build-debug/CMakeFiles/HackersNest.dir/UbiGame_FlappBird/Source/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../SFML-2.5.0/examples/shader"
+  "../SFML-2.5.0/Frameworks"
+  "../SFML-2.5.0/Frameworks/SFML.framework"
+  "../SFML-2.5.0/Frameworks/SFML.framework/Versions"
+  "../SFML-2.5.0/Frameworks/SFML.framework/Versions/2.5.0"
+  "../SFML-2.5.0/Frameworks/SFML.framework/Versions/2.5.0/Headers"
+  "../SFML-2.5.0/Frameworks/SFML.framework/Versions/2.5.0/Headers/Audio"
+  "../SFML-2.5.0/Frameworks/SFML.framework/Versions/2.5.0/Headers/Graphics"
+  "../SFML-2.5.0/Frameworks/SFML.framework/Versions/2.5.0/Headers/Network"
+  "../SFML-2.5.0/Frameworks/SFML.framework/Versions/2.5.0/Headers/System"
+  "../SFML-2.5.0/Frameworks/SFML.framework/Versions/2.5.0/Headers/Window"
+  "../SFML-2.5.0/include"
+  "../SFML-2.5.0/include/SFML"
+  "../SFML-2.5.0/include/SFML/Audio"
+  "../SFML-2.5.0/include/SFML/Graphics"
+  "../SFML-2.5.0/include/SFML/Network"
+  "../SFML-2.5.0/include/SFML/System"
+  "../SFML-2.5.0/include/SFML/Window"
+  "../UbiGame_Blank/Source/Game"
+  "../UbiGame_Blank/Source/GameEngine"
+  "../UbiGame_Blank/Source/GameEngine/EntitySystem"
+  "../UbiGame_Blank/Source/GameEngine/EntitySystem/Components"
+  "../UbiGame_Blank/Source/GameEngine/Util"
+  "../UbiGame_FlappBird/Source/Game"
+  "../UbiGame_FlappBird/Source/Game/GameComponents"
+  "../UbiGame_FlappBird/Source/Game/GameEntities"
+  "../UbiGame_FlappBird/Source/GameEngine"
+  "../UbiGame_FlappBird/Source/GameEngine/EntitySystem"
+  "../UbiGame_FlappBird/Source/GameEngine/EntitySystem/Components"
+  "../UbiGame_FlappBird/Source/GameEngine/Util"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
