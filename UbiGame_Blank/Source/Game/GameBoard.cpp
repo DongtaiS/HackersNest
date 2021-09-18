4b4495a08dad95d98f011a3a4b0f2ace2144956b
Dongtai <dongtaishi.w@gmail.com>
2021-09-18 11:01:34 -0400

    First commit!

--- UbiGame_Blank/Source/Game/GameBoard.cpp
+++ UbiGame_Blank/Source/Game/GameBoard.cpp
@@ -7,7 +7,7 @@ using namespace Game;
 
 GameBoard::GameBoard()
 {
-
+	CreatePlayer();
 }
 
 
@@ -20,4 +20,15 @@ GameBoard::~GameBoard()
 void GameBoard::Update()
 {	
 	
+}
+
+void GameBoard::CreatePlayer()
+{
+	m_player = new GameEngine::Entity();
+	GameEngine::GameEngineMain::GetInstance()->AddEntity(m_player);
+
+	m_player->SetPos(sf::Vector2f(10.0f, 10.0f));            // <-- Give the position 10,10 to the entity
+	m_player->SetSize(sf::Vector2f(10.0f, 10.0f));           // <-- Give the size 10,10 to the entity
+
+	m_player->AddComponent<GameEngine::RenderComponent>();   // <-- Add a RenderComponent to the entity
 }
